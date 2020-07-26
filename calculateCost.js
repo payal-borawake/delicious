31e2c18dd44708f7a0efb176f1a40258e93beee0
Payal Borawake <payalborawake333@gmail.com>
2020-07-26 20:27:26 +0530

    Intial commit

---
+++ calculateCost.js
@@ -0,0 +1,16 @@
+var data = { burger: 0, pizza: 0 };
+function showList(type) {
+  document.querySelector('#' + type).style.display = 'block';
+
+  var foodInputElements = document.querySelectorAll('#' + type + ' input');
+  foodInputElements.forEach(function (element) {
+    element.addEventListener('change', (event) => {
+      if (event.target.checked) {
+        data[type] = data[type] + parseInt(event.target.value);
+      } else {
+        data[type] = data[type] - parseInt(event.target.value);
+      }
+      document.querySelector('#totalCost').value = data.burger + data.pizza;
+    });
+  });
+}
