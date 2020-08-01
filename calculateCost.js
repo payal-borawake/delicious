45b4a4567182a7262f84969c8a68a340a6e21622
Payal Borawake <payalborawake333@gmail.com>
2020-08-01 16:19:14 +0530

    drink churros and friedchicken added

--- calculateCost.js
+++ calculateCost.js
@@ -1,16 +1,18 @@
-var data = { burger: 0, pizza: 0 };
-function showList(type) {
+
+var data = { burger: 0, pizza: 0,drink: 0, churros: 0, friedchicken: 0  };
+function showList(type)
+ {
   document.querySelector('#' + type).style.display = 'block';
 
   var foodInputElements = document.querySelectorAll('#' + type + ' input');
-  foodInputElements.forEach(function (element) {
-    element.addEventListener('change', (event) => {
-      if (event.target.checked) {
+     foodInputElements.forEach(function (element){
+      element.addEventListener('change', (event) => {
+       if (event.target.checked) {
         data[type] = data[type] + parseInt(event.target.value);
-      } else {
+      }else {
         data[type] = data[type] - parseInt(event.target.value);
       }
-      document.querySelector('#totalCost').value = data.burger + data.pizza;
+document.querySelector('#totalCost').value=data.burger+ data.pizza+data.drink+data.churros+data.friedchicken;
     });
   });
 }
